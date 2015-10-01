53093e691a6fe99f4fc9cfec93d382bc9662248f
antixaker <proskuraaleksandr93@gmail.com>
2015-10-01 18:07:23 +0300

    CrossZero project in process

---
+++ ControlWork/ControlWork/ControlWork/ViewModel/CZViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ControlWork.ViewModel
+{
+    class CZViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        CrossZero currentSymbol;
+        public CrossZero CurrentSymbol
+        {
+            protected set
+            {
+                if (currentSymbol != value)
+                {
+                    currentSymbol = value;
+                    //OnPropertyChanged(nameof(currentSymbol));
+                }
+            }
+
+            get { return currentSymbol; }
+        }
+
+        public string SymbolToButton = "";
+
+        public CZViewModel()
+        {
+            SetCurrentActionToZero = new Command(() =>
+            {
+                CurrentSymbol = CrossZero.Zero;
+            });
+
+            SetCurrentActionToCross = new Command(() =>
+            {
+                CurrentSymbol = CrossZero.Cross;
+            });
+
+        }
+
+        //public Command TapGameButton
+        //{
+        //    get
+        //    {
+        //        return _tapGameButton ?? _tapGameButton = new Command(()=> {
+        //            SymbolToButton= CurrentSymbol == CrossZero.Cross ? "X" : "O";
+        //        });
+        //            //
+        //    }
+        //}
+
+        public Command SetCurrentActionToZero;
+        public Command SetCurrentActionToCross;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+
+    enum CrossZero
+    {
+        Cross,
+        Zero
+    }
+}
